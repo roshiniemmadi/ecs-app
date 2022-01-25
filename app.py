c634232447016dfbcf5d18d5dd5c6bfd77ef2d55
roshiniemmadi <97740417+roshiniemmadi@users.noreply.github.com>
2022-01-24 23:19:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@
 # https://github.com/mmulqueen/pyStrich
 from pystrich.datamatrix import DataMatrixEncoder
 
-encoder = DataMatrixEncoder('This is a sample python deployment.')
+encoder = DataMatrixEncoder('This is a sample python deployment for ecs.')
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
